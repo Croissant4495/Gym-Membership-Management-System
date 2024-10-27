7f0b3a79186cadf0e1adf3b563b325cd3131396e
omar waleed <155390963+omar-waleed17@users.noreply.github.com>
2024-10-27 17:59:06 +0300

    finished the trainer database class

--- Gym/src/gym/TrainerDatabase.java
+++ Gym/src/gym/TrainerDatabase.java
@@ -4,8 +4,11 @@
  */
 package gym;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -14,28 +17,90 @@ import java.util.Scanner;
  * @author user
  */
 public class TrainerDatabase {
-     private ArrayList<Trainer> records;
+
+    private ArrayList<Trainer> records;
     private String filename;
 
     public TrainerDatabase(String filename) {
         this.filename = filename;
-       this.records = new ArrayList<>();
+        this.records = new ArrayList<>();
     }
-  public void readFromFile(String filename) {
-        try (Scanner scanner = new Scanner(new File(filename))) {
+
+    public void readFromFile(String filename) {
+        try {
+            Scanner scanner = new Scanner(new File(filename));
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 Trainer trainer = createRecordFrom(line);
                 records.add(trainer);
             }
         } catch (FileNotFoundException e) {
-            e.printStackTrace(); 
+            e.printStackTrace();
         }
     }
- public  Trainer createRecordFrom (String line)
-  {
-      Trainer trainer=null;
-  }
 
-   
+    public Trainer createRecordFrom(String line) {
+
+        String[] stringSplitter = line.split(",");
+        String id = stringSplitter[0];
+        String name = stringSplitter[1];
+        String email = stringSplitter[2];
+        String speciality = stringSplitter[3];
+        String phonenumber = stringSplitter[4];
+        Trainer trainer = new Trainer(id, name, email, speciality, phonenumber);
+        return trainer;
+    }
+
+    public ArrayList<Trainer> returnAllrecords() {
+        return records;
+    }
+
+    public Boolean contains(String key) {
+        for (int i = 0; i < records.size(); i++) {
+            if (records.get(i).getSearchKey().equals(key)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public Trainer getRecord(String key) {
+        Trainer gettingTrainer = null;
+        for (int i = 0; i < records.size(); i++) {
+            if (records.get(i).getSearchKey().equals(key)) {
+                gettingTrainer = records.get(i);
+                return gettingTrainer;
+            }
+        }
+        return gettingTrainer;
+    }
+
+    public void insertRecord(Trainer record) {
+        if (!records.contains(record)) {
+            records.add(record);
+        }
+    }
+
+    public void deleteRecord(String key) {
+        for (int i = 0; i < records.size(); i++) {
+            if (records.get(i).getSearchKey().equals(key)) {
+                records.remove(i);
+                break;
+            }
+        }
+    }
+
+    public void saveToFile() {
+        try (BufferedWriter myWriter = new BufferedWriter(new FileWriter(this.filename))) {
+            for (Trainer trainer : records) {
+                myWriter.write(trainer.LineRepresentation());
+                myWriter.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
 }
