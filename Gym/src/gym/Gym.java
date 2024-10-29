b13211d79771f44030a539674d3aa5129afcd9ad
omar waleed <155390963+omar-waleed17@users.noreply.github.com>
2024-10-29 11:39:21 +0300

    implemented main and menus for admin

--- Gym/src/gym/Gym.java
+++ Gym/src/gym/Gym.java
@@ -67,24 +67,74 @@ public class Gym {
             }
 
         }
+    }
+
+    public static int AdminMenu() {
+        System.out.println("Choose one of the following options:");
+        System.out.println("1. add Trainer");
+        System.out.println("2. get list of Trainers");
+        System.out.println("3. remove trainer");
+        System.out.println("4.logout");
+        return inputNum();
+    }
+
+    public static void Adminloop(AdminRole admin) {
+        int option = AdminMenu();
+        switch (option) {
+
+            case 1:
+                addTrainer(admin);
+                break;
+            case 2:
+                ArrayList<Trainer> ourTrainers=getlistofTrainers(admin);
+                break;
+            case 3:
+                removetrainer(admin);
+                break;
+            case 4:
+                logout(admin);
+            default:
+                System.out.println("Invalid option. Please try again.")
+                break;
         }
-    
-    public static int inputNum(){
+    }
+
+    public void addTrainer(AdminRole admin) {
+        System.out.println("please enter your trainer details");
+        System.out.println("please enter your trainer id");
+        String trainerId = inputLine();
+        System.out.println("please enter your trainer name");
+        String Name = inputLine();
+        System.out.println("please enter your trainer email");
+        String email = inputLine();
+        System.out.println("please enter your trainer speciality");
+        String speciality = inputLine();
+        System.out.println("please enter your trainer phoneNumber");
+        String phoneNumber = inputLine();
+        admin.addTrainer(trainerId, Name, email, speciality, phoneNumber);
+
+    }
+    public void getlistoftrainers(AdminRole admin)
+    {
+        return admin.getListofTrainers();
+    }
+
+    public static int inputNum() {
         Scanner myScan = new Scanner(System.in);
         return Integer.parseInt(myScan.nextLine().replaceAll(" ", ""));
     }
-    
-    public static double inputNumD(){
+
+    public static double inputNumD() {
         Scanner myScan = new Scanner(System.in);
         return Double.parseDouble(myScan.nextLine().replaceAll(" ", ""));
     }
-    
-    public static String inputLine(){
+
+    public static String inputLine() {
         Scanner myScan = new Scanner(System.in);
         return myScan.nextLine().trim();
     }
- 
-    public static int trainerMenu(){
+
+    public static int trainerMenu() {
         System.out.println("Choose one of the following options:");
         System.out.println("1)Add Member");
         System.out.println("2)Get list of Members");
@@ -96,10 +146,10 @@ public class Gym {
         System.out.println("8)Logout");
         return inputNum();
     }
-    
-    public static void trainerLoop(TrainerRole myT){
+
+    public static void trainerLoop(TrainerRole myT) {
         int option = trainerMenu();
-        
+
     }
 
 }
